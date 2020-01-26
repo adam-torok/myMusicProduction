c3c054c31ecc47bbfef8a3538bb5c2bab88e127b
adam-torok <adam-torok@outlook.hu>
2020-01-26 13:10:50 +0100

    Make fonts pulled by not cdn but downloaded.

--- PHP/COMPONENTS/headerMeta.php
+++ PHP/COMPONENTS/headerMeta.php
@@ -4,7 +4,6 @@
 <meta http-equiv="X-UA-Compatible" content="ie=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1, content=" width=1000 " maximum-scale=1, user-scalable=no"/>
 <link rel="stylesheet" href="../CSS/header.css">
-<link rel="stylesheet" href="../CSS/fonts.css">
 <link rel="stylesheet" href="../CSS/footer.css">
 <link rel="stylesheet" href="../CSS/form.css">
 <link rel="stylesheet" href="../CSS/modal.css">
@@ -14,13 +13,10 @@
 <link rel="stylesheet" href="../CSS/music-related.css">
 <link rel="stylesheet" href="../CSS/easy-autocomplete.min.css">
 <link rel="stylesheet" href="../CSS/main.css">
+<link rel="stylesheet" href="../CSS/fonts.css">
 <link rel="stylesheet" href="../CSS/table.css">
 <link rel="stylesheet" href="../CSS/lightmode.css">
 <link rel="icon" type="image/png" href="../img/compact-disc.png">
 <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
-<link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
-<link href="https://fonts.googleapis.com/css?family=Big+Shoulders+Text&display=swap" rel="stylesheet">
-<link href="https://fonts.googleapis.com/css?family=Montserrat+Subrayada&display=swap" rel="stylesheet">
-<link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet">
 <script src="../JS/jquery-3.4.1.min.js" charset="utf-8"></script>
 <script src="../JS/main.js" charset="utf-8"></script>
