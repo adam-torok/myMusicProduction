b49d64edbda99b9d3c46050e93ef954b4e8857e4
adam-torok <adam-torok@outlook.hu>
2020-01-26 13:33:23 +0100

    commenting

--- PHP/ADMIN/adminHeaderMeta.php
+++ PHP/ADMIN/adminHeaderMeta.php
@@ -12,10 +12,5 @@
         <link rel="stylesheet" href="\mymusic\CSS\spinner.css">
         <link rel="stylesheet" href="\mymusic\CSS\music-related.css">
         <link rel="icon" type="image/png" href="../img/compact-disc.png">
-        <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
-        <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
-        <link href="https://fonts.googleapis.com/css?family=Big+Shoulders+Text&display=swap" rel="stylesheet">
-        <link href="https://fonts.googleapis.com/css?family=Montserrat+Subrayada&display=swap" rel="stylesheet">
-        <link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet">
         <script src="https://kit.fontawesome.com/75ad4010ea.js" crossorigin="anonymous"></script>
         <script src="../JS/jquery-3.4.1.min.js" charset="utf-8"></script>
